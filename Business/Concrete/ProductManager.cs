0e4a03f7f55ae4fba7823e1cdb71211c94c6b375
utku berk gök <utku_19_berk@hotmail.com>
2021-03-17 16:30:19 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,25 @@
+﻿using Business.Abstract;
+using DataAccsess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IPoductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            //iş kodlarımızı yazıyoruz buraya
+            return _productDal.GetAll();
+        }
+    }
+}
