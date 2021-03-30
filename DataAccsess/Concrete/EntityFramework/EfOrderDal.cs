756637d0a1723f9774a60e910fa3f165447b6ea9
utku berk gök <utku_19_berk@hotmail.com>
2021-03-30 17:36:09 +0300

    6 Şubat 2.mola

--- DataAccsess/Concrete/EntityFramework/EfOrderDal.cs
+++ DataAccsess/Concrete/EntityFramework/EfOrderDal.cs
@@ -1,17 +1,13 @@
 ﻿using Core.DataAccess.EntityFramework;
 using DataAccsess.Abstract;
 using Entities.Concrete;
-using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
 using System.Text;
 
 namespace DataAccsess.Concrete.EntityFramework
 {
-    public class EfProductDal : EfEntityRepositoryBase<Product, NorthwindContext>, IProductDal
+    public class EfOrderDal : EfEntityRepositoryBase<Order, NorthwindContext>, IOrderDal
     {
-        
     }
 }
