0ab0c3f49daafc76f71446200bd25cfbb06841de
utku berk gök <utku_19_berk@hotmail.com>
2021-04-01 21:22:14 +0300

    13 Şubat 1. Commit

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccsess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        //Loosely coupled - gevşek bağlılık
+        //naming convention
+        //IoC Container -- Inversion of Control
+        IPoductService _productService;
+
+        public ProductsController(IPoductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public List<Product> Get()
+        {
+            //Dependency chain
+            var result = _productService.GetAll();
+            return result.Data;
+        }
+    }
+}
